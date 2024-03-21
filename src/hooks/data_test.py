b6b88e80c776b18f7a99a6431936c495c7508a95
Katelyn Gigante <clockwork.singularity@gmail.com>
2024-03-21 15:41:15 +1100

    Workflow updates

--- src/hooks/data_test.py
+++ src/hooks/data_test.py
@@ -1,5 +1,13 @@
-from test.TestBase import WorldTestBase
+from test.bases import WorldTestBase
 
-class XIVTest(WorldTestBase):
+class FatesanityTest(WorldTestBase):
     game = "Manual_FFXIV_Silasary"
 
+    fatesanity = True
+
+class FishsanityTest(WorldTestBase):
+    game = "Manual_FFXIV_Silasary"
+
+    fishsanity = True
+    fishsanity_big_fish = True
+    fishsanity_timed_fish = True
