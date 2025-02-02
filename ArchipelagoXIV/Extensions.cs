73a9689177a75ad32653022db8353735649b78ce
Katelyn Gigante <clockwork.singularity@gmail.com>
2025-02-02 13:53:30 +1100

    Support all the default colours
    I might want to tweak the mappings though?

--- ArchipelagoXIV/Extensions.cs
+++ ArchipelagoXIV/Extensions.cs
@@ -18,23 +18,30 @@ namespace ArchipelagoXIV
         {
             if (color == Color.Red)
                 return 17;
-            if (color == Color.Blue)
-                return 38;
             if (color == Color.Green)
                 return 45;
             if (color == Color.Yellow)
                 return 62;
-            if (color == Color.Plum)
-                return 49;
-            if (color == Color.SlateBlue)
-                return 57;
+            if (color == Color.Blue)
+                return 38;
+            if (color == Color.Magenta)
+                return 522;
             if (color == Color.Cyan)
                 return 69;
+            if (color == Color.Black)
+                return 0;
+            if (color == Color.White)
+                return 1;
+            if (color == Color.SlateBlue)
+                return 57;
             if (color == Color.Salmon)
                 return 537;
-            DalamudApi.PluginLog.Error($"Unknown color {color}");
+            if (color == Color.Plum)
+                return 49;
+            DalamudApi.PluginLog.Error($"Unknown color {color.R},{color.G},{color.B}");
             return 1;
-
+            //var v1 = new Vector4(color.R, color.G, color.B, 255);
+            //return (ushort)ImGui.ColorConvertFloat4ToU32(v1);
         }
     }
 }
