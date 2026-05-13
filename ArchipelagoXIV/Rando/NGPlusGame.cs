eacc2bb61443c643b0d5cc158f846a8b3154af1d
Katelyn Gigante <clockwork.singularity@gmail.com>
2026-05-13 14:04:41 +1000

    Handle slots that predate the world_version slot info.

--- ArchipelagoXIV/Rando/NGPlusGame.cs
+++ ArchipelagoXIV/Rando/NGPlusGame.cs
@@ -57,8 +57,10 @@ namespace ArchipelagoXIV.Rando
 
             // Boss goals were added in version 0.29.0, previously "Defeat Shinryu" was goal 1.
             // It moved to index 4
-            var worldVersion = Version.Parse((string)slotData["world_version"]);
-            DalamudApi.PluginLog.Information($"Unparsed world version: {slotData["world_version"]}");
+            slotData.TryGetValue("world_version", out var version_obj);
+            var version_string = version_obj as string ?? "0.0.0";
+            var worldVersion = Version.Parse(version_string);
+            DalamudApi.PluginLog.Information($"Unparsed world version: {version_string}");
 
             if (worldVersion < new Version(0, 29, 0))
             {
