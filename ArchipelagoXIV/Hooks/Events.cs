29223bedca39e411589272fbbf5786190d006b91
shonicth <shonicth@gmail.com>
2026-06-22 15:44:15 -0400

    Support for collectables and HQ in inventory
    This checks for all items, not just NQ. By default this will also output the NQ string from HQ/Collectable, so no need to strip this off

--- ArchipelagoXIV/Hooks/Events.cs
+++ ArchipelagoXIV/Hooks/Events.cs
@@ -30,12 +30,11 @@ namespace ArchipelagoXIV.Hooks
 
         private void GameInventory_ItemAdded(Dalamud.Game.Inventory.GameInventoryEvent type, Dalamud.Game.Inventory.InventoryEventArgTypes.InventoryEventArgs data)
         {
-            if (Data.Items.TryGetValue(data.Item.ItemId, out var value))
+            if (Data.Items.TryGetValue(data.Item.BaseItemId, out var value))
             {
                 var name = value.Name.ToString().TrimEnd();
                 if (APData.FishData.ContainsKey(name))
                 {
-                    //DalamudApi.Echo($"Caught a {name}!");
                     var loc = apState.MissingLocations.FirstOrDefault(l => l.Name == name);
                     if (loc != null && loc.IsAccessible())
                     {
