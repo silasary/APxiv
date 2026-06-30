5e6b0865f6f6ed3e806fb0cdf86d6bfb4b0f515b
Marpfie <24807827+Marpfie@users.noreply.github.com>
2026-07-01 01:06:41 +0200

    feat: boss key default 10/max 25

--- src/hooks/Options.py
+++ src/hooks/Options.py
@@ -206,14 +206,14 @@ class BossKeyPieces(Range):
     Number of key pieces required to challenge the final boss (boss goal only).
 
     When set to 0, no key is required and the victory location is accessible on region and level alone.
-    When set to 1-10, that many key items are shuffled into the item pool
+    When set to 1-25, that many key items are shuffled into the item pool
     and must all be collected before the goal location is accessible.
     Has no effect when the goal is set to McGuffin Hunt (Collect Memories).
     """
     display_name = "Boss Key Pieces"
-    default = 0
+    default = 10
     range_start = 0
-    range_end = 10
+    range_end = 25
 
 class ForceJob(OptionSet):
     """
