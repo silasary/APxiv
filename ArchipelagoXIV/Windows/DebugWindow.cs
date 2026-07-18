a0cf03f8924f626349767f850a04f5778d857c4a
Katelyn Gigante <clockwork.singularity@gmail.com>
2026-07-18 21:11:41 +1000

    Show reachable regions in the debug window

--- ArchipelagoXIV/Windows/DebugWindow.cs
+++ ArchipelagoXIV/Windows/DebugWindow.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ArchipelagoXIV.Rando;
 
 namespace ArchipelagoXIV.Windows
 {
@@ -13,6 +14,14 @@ namespace ArchipelagoXIV.Windows
         public override void Draw()
         {
             ImGui.Text($"apState.territoryName: `{state.territoryName}`");
+
+            ImGui.Separator();
+            var regions = APData.Regions.OrderBy(r => r.Value.Distance ?? 999).ToList();
+            foreach (var region in regions)
+            {
+                var distance = region.Value.Distance ?? 99;
+                ImGui.Text($"{distance.ToString("D2")}: {region.Key}");
+            }
         }
     }
 }
