a762159fce0bde613224bbaf17a74209a6596387
shonicth <shonicth@gmail.com>
2026-07-27 20:06:31 -0400

    added a check for if it contains or. If it does not, it will try to confirm that it contains all

--- ArchipelagoXIV/Rando/Logic.cs
+++ ArchipelagoXIV/Rando/Logic.cs
@@ -23,7 +23,10 @@ namespace ArchipelagoXIV.Rando
             var rules = (from Match m in Regexes.itemRegex.Matches(requires)
                          select HasItem(m.Groups[0].Value)).ToArray();
             if (rules.Length != 0)
-                return (state, asCurrentClass) => rules.Any(r => r(state, asCurrentClass));
+                if(requires.Contains(" or "))
+                    return (state, asCurrentClass) => rules.Any(r => r(state, asCurrentClass));
+                else
+                    return (state, asCurrentClass) => rules.All(r => r(state, asCurrentClass));
             if (string.IsNullOrEmpty(requires))
                 return Always();
             DalamudApi.Echo($"Could not parse Requires string: {requires}");
