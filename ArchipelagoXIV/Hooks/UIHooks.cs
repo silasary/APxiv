ec4030f67ce96f2715d3e403685d0b2e63d0198c
Katelyn Gigante <clockwork.singularity@gmail.com>
2026-08-11 10:16:00 +1000

    Not sure why this worked on my machine but not on CI, but ah well.  Easy fix.

--- ArchipelagoXIV/Hooks/UIHooks.cs
+++ ArchipelagoXIV/Hooks/UIHooks.cs
@@ -93,7 +93,8 @@ namespace ArchipelagoXIV.Hooks
                         icon.AttachNode(targetNode, NodePosition.AfterTarget);
                     }
                     //DalamudApi.PluginLog.Debug($"Setting icon visibility for {componentNode->NodeId} ({name}) to {visible}");
-                    icon?.Node->ToggleVisibility(visible); 
+                    if (icon != null)
+                        icon.Node->ToggleVisibility(visible);
 
                     // todo: Replace the texture, maybe check if it's hinted?
                     //if (hints.Contains(loc.ApId))
