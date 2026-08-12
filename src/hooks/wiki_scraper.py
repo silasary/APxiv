87586c0a7c0795c7c8b521ac0832a81dc3cae1f6
shonicth <shonicth@gmail.com>
2026-08-12 11:03:56 -0400

    Added fields for the scraper to be per hole

--- src/hooks/wiki_scraper.py
+++ src/hooks/wiki_scraper.py
@@ -650,9 +650,13 @@ def apply_bait() -> None:
         if fish.get('tribal'):
             continue
         fish['zones'] = {}
+        fish['logical_bait'] = {}
         fish['all_bait'] = {}
+        fish['all_bait_by_hole'] = {}
         fish['logical_intuition'] = {}
+        fish['logical_intuition_by_hole'] = {}
         fish['intuition_bait'] = {}
+        fish['intuition_bait_by_hole'] = {}
 
         for hole, baits in bait_paths.get(name, {}).copy().items():
             if not baits:
@@ -724,9 +728,13 @@ def apply_bait() -> None:
                                                 if lookup_item_ui_category(item["ItemUICategory"]) != "Seafood":
                                                     moochstatus = False
                             fish['intuition_bait'][zone_name] = intuition_bait
+                            fish['intuition_bait_by_hole'][hole] = intuition_bait
                             fish['logical_intuition'].setdefault(zone_name, []).append(logical_intuition)
+                            fish['logical_intuition_by_hole'].setdefault(hole, []).append(logical_intuition)
                             fish['intuition_bait'][zone_name] = sorted(set(fish['intuition_bait'][zone_name]))
+                            fish['intuition_bait_by_hole'][hole] = sorted(set(fish['intuition_bait_by_hole'][hole])) 
                             fish['logical_intuition'][zone_name] = sorted(set(fish['logical_intuition'][zone_name]))
+                            fish['logical_intuition_by_hole'][hole] = sorted(set(fish['logical_intuition_by_hole'][hole]))
             for bait in baits.copy():
                 if isinstance(bait, list):
                     baits.remove(bait)
@@ -788,10 +796,14 @@ def apply_bait() -> None:
 
             if baits:
                 fish['zones'].setdefault(zone_name, []).append(teamcraft_optimalbait)
+                fish['logical_bait'].setdefault(hole, []).append(teamcraft_optimalbait)
                 fish['all_bait'][zone_name] = baits
+                fish['all_bait_by_hole'][hole] = baits
                 #sort and clean
                 fish['zones'][zone_name] = sorted(set(fish['zones'][zone_name]))
+                fish['logical_bait'][hole] = sorted(set(fish['logical_bait'][hole]))
                 fish['all_bait'][zone_name] = sorted(set(fish['all_bait'][zone_name]))
+                fish['all_bait_by_hole'][hole] = sorted(set(fish['all_bait_by_hole'][hole]))  
                 #Merge zones, comment this out for poptracker scraper
             else:
                 print(f"No bait for {name} in {hole}")
